95343c56bbd44e609ada4b612c064f7f3d943d85
Evan <45468330+EvanG13@users.noreply.github.com>
2023-10-26 08:19:50 -0700

    feat: unauthorized homepage view (#3, PR #4)
    * feat: unauthorized homepage view (#3)
    
    * chore: run prettier

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,15 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_request, response) => {
-  const teamLetter = "D";
-  const teamMembers = [
-    "Ahmar Huda",
-    "Caimin Rybolt-O'Keefe",
-    "Cleveland Plonsey",
-    "Evan Giampaoli",
-  ];
-
-  response.render("root", { teamLetter, teamMembers });
+  response.render("home.ejs");
 });
 
 router.get("/login", (_request, response) => {
