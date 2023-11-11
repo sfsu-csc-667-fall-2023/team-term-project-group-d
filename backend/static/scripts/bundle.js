ab70edaa5a4f0fabe427be4e28ce6805a77d5e03
EvanG13 <emgiampaoli@gmail.com>
2023-11-10 22:03:23 -0800

    chore: bundle.js changes (#23)

--- backend/static/scripts/bundle.js
+++ backend/static/scripts/bundle.js
@@ -1,1 +1,7 @@
-console.log("Hello from a bundled asset.");
+console.log("Hello from a bundled asset."),
+  document.getElementById("register-button").addEventListener("click", (e) => {
+    document.getElementById("password").value !==
+      document.getElementById("confirm-password").value &&
+      ((document.getElementById("match-password-card").style.display = "block"),
+      e.preventDefault());
+  });
