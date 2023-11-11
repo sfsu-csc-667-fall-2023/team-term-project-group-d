3f8c81443597b8961f32b72be82b0f56c8bd0258
EvanG13 <emgiampaoli@gmail.com>
2023-11-10 21:44:06 -0800

    feat: cleanup register error views (#23)

--- frontend/register.js
+++ frontend/register.js
@@ -1,9 +1,18 @@
-document.getElementById("registerButton").addEventListener("click", (event) => {
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirm-password").value;
+document
+  .getElementById("register-button")
+  .addEventListener("click", (event) => {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
 
-  if (password !== confirmPassword) {
-    document.getElementById("match-password-card").style.display = "block";
-    event.preventDefault();
+    if (password !== confirmPassword) {
+      document.getElementById("match-password-card").style.display = "block";
+      event.preventDefault();
+    }
+  });
+
+function closeErrorCard(cardId) {
+  const errorCard = document.getElementById(cardId);
+  if (errorCard) {
+    errorCard.style.display = "none";
   }
-});
+}
