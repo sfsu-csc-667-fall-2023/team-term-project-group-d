df4e2f355b5a3128dc2e043dbba311862d4ab1a8
CleveyP <clevelandplonsey@gmail.com>
2023-11-12 15:47:44 -0800

    add to bundle.js

--- backend/static/scripts/bundle.js
+++ backend/static/scripts/bundle.js
@@ -1,1 +1,29 @@
-console.log("Hello from a bundled asset.");
+console.log("Hello from a bundled asset."),
+  (() => {
+    let e;
+    const t = document.getElementsByClassName("hand-card");
+    for (let l = 0; l < t.length; l++)
+      t.item(l).addEventListener("click", (l) => {
+        e = l.target.getAttribute("cardId");
+        for (let e = 0; e < t.length; e++)
+          t.item(e) != l.target && t.item(e).classList.remove("selected");
+        l.target.classList.toggle("selected");
+      });
+    document
+      .getElementById("play-button")
+      .addEventListener("click", async (t) => {
+        t.preventDefault();
+        const l = { cardId: e, color: "red", userId: 21 };
+        try {
+          const e = await fetch("http://localhost:3000/game/3/card/play", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(l),
+            }),
+            t = await e.json();
+          console.log(t);
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  })();
