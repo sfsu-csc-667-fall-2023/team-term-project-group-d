0626ffa330c30462c603ed40938ac301619a455d
EvanG13 <emgiampaoli@gmail.com>
2023-11-13 14:50:30 -0800

    feat: update error views for register and login (#23)

--- backend/db/session.js
+++ backend/db/session.js
@@ -13,7 +13,7 @@ const sessionConfig = expressSession({
   }),
   secret: process.env.COOKIE_SECRET || "csc667-team-d",
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
   cookie: {
     maxAge: parseInt(process.env.COOKIE_MAX_AGE) || 10 * 24 * 60 * 60 * 1000,
   },
@@ -23,6 +23,13 @@ const sessionConfig = expressSession({
 const setLocalUserData = (req, res, next) => {
   if (req.session.user) res.locals.user = req.session.user;
   else res.locals.user = null;
+
+  if (req.session.errors) {
+    res.locals.errors = req.session.errors;
+    delete req.session.errors;
+  } else {
+    res.locals.errors = null;
+  }
 
   next();
 };
