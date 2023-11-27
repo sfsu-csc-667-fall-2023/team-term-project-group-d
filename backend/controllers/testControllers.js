35c8b44f89992926a878934dd73a483a10a41c79
CleveyP <clevelandplonsey@gmail.com>
2023-11-26 18:24:59 -0800

    feat: frontend checks user's move if legal and lets user pick color for a wild card.

--- backend/controllers/testControllers.js
+++ backend/controllers/testControllers.js
@@ -1,9 +1,11 @@
 const getGame = async (req, res) => {
   res.render("game.ejs", {
+    gameId: 1,
     clientHand: [
       { color: "red", symbol: "draw_two", id: 1 },
       { color: "yellow", symbol: "three", id: 2 },
       { color: "red", symbol: "four", id: 3 },
+      { color: "wild", symbol: "wild", id: 4 },
     ],
     playerList: [
       { name: "cleveland", handSize: 4 },
