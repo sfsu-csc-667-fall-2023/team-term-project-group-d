387bd3451356abb57e566207cd618587c7c6c3fe
CleveyP <clevelandplonsey@gmail.com>
2023-11-30 23:48:22 -0800

    add lobby rout to routers in index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,6 @@ const root = require("./root");
 const user = require("./user");
 const game = require("./game");
 const chat = require("./chat");
-//const lobby = require("./lobby");
+const lobby = require("./lobby");
 
-module.exports = { root, user, game, chat };
+module.exports = { root, user, game, chat, lobby };
