c2fcfe3725fd3d877f67e9cf3b227ebc4bbb0107
CaiminRO <caiminr01@gmail.com>
2023-12-01 23:53:24 -0800

    Frontend displays Lobbies and has filtering

--- frontend/home.js
+++ frontend/home.js
@@ -1,53 +1,33 @@
-// TODO : request list of available games
-const games = [
-  {
-    id: 1,
-    name: "Test",
-    max_players: 4,
-    lobby_size: 1,
-  },
-  {
-    id: 2,
-    name: "Test again",
-    max_players: 4,
-    lobby_size: 2,
-  },
-  {
-    id: 3,
-    name: "Test again again",
-    max_players: 4,
-    lobby_size: 3,
-  },
-];
+/// Get List of Lobbies
+// Game List container
+const gameList = document.getElementById("game-list");
 
-const joinGame = async (gameId) => {
+const getLobbies = async () => {
   try {
-    const res = await fetch("/game/join", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ gameId: gameId }),
-    });
+    const res = await fetch(`/lobby/list`);
     const json = await res.json();
-    console.log(json);
+
+    // game { id, name, player_count, max_players, has_password }
+    return json;
   } catch (error) {
     console.log(error);
+    return [];
   }
 };
 
-games.forEach((game) => {
+const displayGameInList = (game) => {
   const outerDiv = document.createElement("div");
   outerDiv.classList.add("available-game");
+  outerDiv.id = `game-${game.id}`;
 
   const idParagraph = document.createElement("p");
   idParagraph.innerText = game.id;
 
   const nameParagraph = document.createElement("p");
-  nameParagraph.innerText = game.name;
+  nameParagraph.innerText = game.name + (game.has_password ? "🔒" : "");
 
   const lobbySizeParagraph = document.createElement("p");
-  lobbySizeParagraph.innerText = `Players : ${game.lobby_size}/${game.max_players}`;
+  lobbySizeParagraph.innerText = `Players : ${game.player_count}/${game.max_players}`;
 
   const joinButton = document.createElement("button");
   joinButton.addEventListener("click", () => {
@@ -64,8 +44,47 @@ games.forEach((game) => {
   outerDiv.appendChild(innerDiv);
   outerDiv.appendChild(joinButton);
 
-  document.getElementById("game-list-container").appendChild(outerDiv);
+  gameList.appendChild(outerDiv);
+};
+
+// Get All Lobbies on page load
+var games = [];
+(async () => {
+  games = await getLobbies();
+
+  games.forEach((game) => displayGameInList(game));
+})();
+
+const buttonListRefresh = document.getElementById("list-refresh-button");
+buttonListRefresh.addEventListener("click", async () => {
+  games = await getLobbies();
+
+  gameList.innerHTML = ""; // empty list body
+
+  games.forEach((game) => displayGameInList(game));
+  displayFilteredList();
 });
+
+/// Join Game
+const joinGame = async (gameId) => {
+  try {
+    const res = await fetch(`/game/${gameId}/join`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ password: "" }),
+      redirect: "follow",
+    });
+
+    let message = await res.text();
+    console.log(message);
+
+    if (res.redirected) window.location.href = res.url;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const menuCreateGameButton = document.getElementById("create-game-button");
 menuCreateGameButton.addEventListener("click", () => {
@@ -80,7 +99,83 @@ closeCreateGameFormButton.addEventListener("click", () => {
 });
 
 const formCreateFormButton = document.getElementById("form-create-game");
-formCreateFormButton.addEventListener("click", (e) => {
+formCreateFormButton.addEventListener("click", async (e) => {
   e.preventDefault();
-  alert("TODO : fix this form & create the game");
+  try {
+    const res = await fetch("/game/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, password, max_players }),
+    });
+    let message = await res.text();
+    console.log(message);
+
+    if (res.redirected) window.location.href = res.url;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/// Lobby List Filtering
+// Lobby List Filters
+const buttonListFilterAll = document.getElementById("list-filter-all");
+const buttonListFilterHideFull = document.getElementById(
+  "list-filter-hide-full",
+);
+const buttonListFilterPublic = document.getElementById("list-filter-public");
+const buttonListFilterPrivate = document.getElementById("list-filter-private");
+
+// Default filter states (on page refresh / load)
+buttonListFilterAll.checked = true;
+buttonListFilterHideFull.checked = false;
+buttonListFilterPublic.checked = true;
+buttonListFilterPrivate.checked = true;
+
+const displayFilteredList = () => {
+  const hideFull = buttonListFilterHideFull.checked;
+  const showPublic = buttonListFilterPublic.checked;
+  const showPrivate = buttonListFilterPrivate.checked;
+
+  games.forEach((game) => {
+    const gameContainer = document.getElementById(`game-${game.id}`);
+
+    if (hideFull && game.player_count == game.max_players)
+      gameContainer.style.display = "none";
+    else if (
+      (!showPrivate && game.has_password) ||
+      (!showPublic && !game.has_password)
+    )
+      gameContainer.style.display = "none";
+    else gameContainer.style.display = "flex";
+  });
+};
+
+buttonListFilterAll.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    buttonListFilterHideFull.checked = false;
+    buttonListFilterPublic.checked = true;
+    buttonListFilterPrivate.checked = true;
+
+    displayFilteredList();
+  }
+});
+
+buttonListFilterHideFull.addEventListener("change", (event) => {
+  if (event.target.checked) buttonListFilterAll.checked = false;
+
+  displayFilteredList();
+});
+
+buttonListFilterPublic.addEventListener("change", (event) => {
+  if (!event.target.checked) buttonListFilterAll.checked = false;
+
+  displayFilteredList();
+});
+
+buttonListFilterPrivate.addEventListener("change", (event) => {
+  if (!event.target.checked) buttonListFilterAll.checked = false;
+
+  displayFilteredList();
 });
