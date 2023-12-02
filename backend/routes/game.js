7e6bf67d0d364f694a2365d339a3468e2e3e4410
CaiminRO <caiminr01@gmail.com>
2023-12-01 23:52:12 -0800

    Adds APIs: get Lobby list and create / join a Lobby

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const gameRouter = express.Router();
-const { playCard, getGame } = require("../controllers/gameControllers");
+const {
+  playCard,
+  getGame,
+  createGame,
+  joinGame,
+  getMyGames,
+} = require("../controllers/gameControllers");
 
 gameRouter.post("/:id/card/play", playCard);
+
+gameRouter.post("/create", createGame);
+
+gameRouter.post("/:id/join", joinGame);
+
+gameRouter.get("/mine", getMyGames);
 
 gameRouter.get("/:id", getGame);
 
