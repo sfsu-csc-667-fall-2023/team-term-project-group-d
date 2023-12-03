b7111ab5d6794e57ab929809036434b107da4da4
ahuda620 <ahuda620@gmail.com>
2023-12-03 09:02:59 -0800

    added socket io client listener that broadcasts a message to the chatbox when someone has joined the game room to other users in the room

--- frontend/games/index.js
+++ frontend/games/index.js
@@ -1,1 +1,20 @@
-console.log("games");
+import { io } from "socket.io-client";
+
+const chatWindow = document.querySelector("#chat-messages");
+const gameId = document.querySelector("#gameId").value;
+
+const gameSocket = io({ query: { id: gameId } });
+
+gameSocket.on("game-join", (userName) => {
+  console.log(`${userName} has joined the game`);
+
+  const div = document.createElement("div");
+  div.classList.add("message");
+
+  const p = document.createElement("p");
+  p.innerText = `${userName} has joined the game`;
+
+  div.appendChild(p);
+
+  chatWindow.appendChild(div);
+});
