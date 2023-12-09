445befae1a1e52e3f0fb64e95c5b83f36dfe0029
CaiminRO <89493663+CaiminRO@users.noreply.github.com>
2023-12-08 18:36:12 -0800

    Cookie secured and Proxy trusted for Render in Production (#81)

--- backend/db/session.js
+++ backend/db/session.js
@@ -15,6 +15,7 @@ const sessionConfig = expressSession({
   resave: false,
   saveUninitialized: true,
   cookie: {
+    secure: process.env.NODE_ENV === "production",
     maxAge: parseInt(process.env.COOKIE_MAX_AGE) || 10 * 24 * 60 * 60 * 1000,
   },
 });
